fcda4d3ff02e439890ebb3f2e9f737b39b9c0bc1
timus1666 <timus1666@gmail.com>
2024-06-08 14:50:36 +0000

    Search Functionality implemented and backtested in ReactJS

--- api-list-view/api-list-view/src/App.js
+++ api-list-view/api-list-view/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from"./logo.svg";
+import "./App.css";
+import {useState} from "react";
 
-function App() {
+const api = {
+  key: '3ffd0a95e1265b4c545a278b20f214eb',
+  base: 'https://api.openweathermap.org/data/2.5/',
+
+};
+
+function App(){
+  const [search,setSearch] = useState("");
+
+  const searchPressed = () => {
+   fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then((res) => res.json())
+    .then((result) => {
+       console.log(result);
+  });
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1>Weather App</h1>
+
+      {/**SEARCH BOX */}
+      <div>
+      <input 
+      type='text'
+      placeholder="Search"
+      onChange={e => setSearch(e.target.value)}
+
+      />
+
+      <button onClick={searchPressed}>Search</button>
+      </div>
+    {/**Location */}
+    <p>New York City,USA</p>
+    {/**Temperature F/C */}
+    <p> 32 * F </p>
+    {/**Codition {SUNNY} */}
+    <p>SUNNY</p>
+    </header>
+      </div>
   );
 }
-
 export default App;
